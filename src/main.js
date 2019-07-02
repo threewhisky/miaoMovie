9adb655f93e92e876957a6b215f7b92a26fbd2f0
threewhisky <threewhisky@163.com>
2019-07-03 00:27:48 +0800

    add setData

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import App from './App.vue'
 import router from './routers'
 import store from './stores'
 
+// 引入axios，并添加到Vue上
+import axios from 'axios'   
+Vue.prototype.axios = axios;  
+
+// 图片的url的过滤器
+Vue.filter('setWH', (url, arg) => {
+  return url.replace(/w\.h/, arg);
+});
+
+
+
 Vue.config.productionTip = false
 
 new Vue({
